c998f0fba581e13fe1b4972c9c21d8e372095916
karenheimer <karen.heimerc@gmail.com>
2024-02-13 13:35:56 -0300

    Novas alterações

--- Atividade03/src/app/classes/matrix2.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Matrix2 } from './matrix2';
-
-describe('Matrix2', () => {
-  it('should create an instance', () => {
-    expect(new Matrix2()).toBeTruthy();
-  });
-});
--- Atividade04/src/app/classes/imagem.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Imagem } from './imagem';
-
-describe('Imagem', () => {
-  it('should create an instance', () => {
-    expect(new Imagem(10, 10)).toBeTruthy();
-  });
-});
